5e60354b095a6e1f181287139df350d636d8337a
Froissart Kevin <kevin.froissart69@gmail.com>
2020-12-16 22:19:42 +0100

    rebase

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text></Text>
+      <Text numberOfLines={1} >Lorris est une MERDE cette phrase est vraiment super longue mais c'est fait exprès</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
